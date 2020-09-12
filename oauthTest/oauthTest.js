c9e919d6097a784d85d13cd4031d99a3b2ffe218
Daniel <32464403+dkallen78@users.noreply.github.com>
2020-09-11 19:45:01 -0500

    failing...

--- oauthTest/oauthTest.js
+++ oauthTest/oauthTest.js
@@ -1,6 +1,16 @@
-fetch("https://www.reddit.com/api/v1/authorize", {
+let id = "KtOleLnFjmFBVA";
+let secret = "uE__pJB__naprJxfJPFqKHzSsfU";
+let userPass64 = btoa(id+':'+secret);
+fetch("https://www.reddit.com/api/v1/access_token", {
   method: "POST",
-
+  body: `grant_type=client_credentials&` +
+        `user=${id}&` +
+        `password=${secret}`,
+  headers: {
+    "Access-Control-Allow-Headers": "Access-Control-Allow-Headers",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Access-Control-Allow-Origin": "https://www.reddit.com"
+  }
 })
   //----------------------------------------------------//
   //Fetches the JSON data from Reddit                   //
@@ -13,5 +23,8 @@ fetch("https://www.reddit.com/api/v1/authorize", {
   .then(function(myJson) {
     console.log(myJson);
     pageData = myJson;
-    crunch(pageData);
+    //crunch(pageData);
+  })
+  .catch(function(err) {
+    console.log(err);
   });
